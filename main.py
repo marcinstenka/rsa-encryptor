7d741e5d5a7530d4123294ff522e415c6e1b6e68
KaraitPL <karaitpl@gmail.com>
2024-04-08 23:02:33 +0200

    Conflicts resolved

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+#Import the required Libraries
+from generator import generate
 from tkinter import *
 from tkinter import ttk
+import runpy
+import sys
 
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+#Create an instance of Tkinter frame
+win = Tk()
+
+#Set the geometry of Tkinter frame
+win.geometry("750x250")
+
+def generateRSA():
+   global entry
+   generate(entry.get())
+
+
+label = Label(win, text="Podaj PIN:")
+label.pack()
+#Create an Entry widget to accept User Input
+entry = Entry(win, width= 40)
+entry.focus_set()
+entry.pack()
+
+#Create a Button to validate Entry Widget
+ttk.Button(win, text= "Generuj parę kluczy",width= 20, command=generateRSA).pack(pady=20)
+
+win.mainloop()
