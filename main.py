dc09bf2115a4cd24e8c6ec4751d2bf0f63d11a3a
KaraitPL <karaitpl@gmail.com>
2024-04-14 17:14:24 +0200

    GUI rework

--- main.py
+++ main.py
@@ -1,29 +1,66 @@
-#Import the required Libraries
-from generator import generate
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
-import runpy
-import sys
+from tkinter.ttk import *
+from tkinter import *
+from generator import generate
 
-#Create an instance of Tkinter frame
-win = Tk()
 
-#Set the geometry of Tkinter frame
-win.geometry("750x250")
+def enterPIN():
+    global entry
+    global pinWindow
+    pinWindow = Toplevel(win)
+    win.eval(f'tk::PlaceWindow {str(pinWindow)} center')
+    pinWindow.title("Enter PIN:")
+    pinWindow.geometry("145x100")
+    Label(pinWindow, text="Enter PIN:").grid(row=0, padx=10, pady=(10, 0))
+    entry = Entry(pinWindow)
+    entry.grid(row=1, pady=0, padx=10)
+    Button(pinWindow, text="Generuj", command=generateRSA).grid(row=2, padx=10, pady=(10, 0))
+
+
 
 def generateRSA():
-   global entry
-   generate(entry.get())
+    global entry
+    global pinWindow
+    global label2
+    if len(entry.get()) == 4:
+        generate(entry.get())
+        pinWindow.destroy()
+        label2.config(text="Klucze wygenerowane poprawnie")
 
 
-label = Label(win, text="Podaj PIN:")
-label.pack()
-#Create an Entry widget to accept User Input
-entry = Entry(win, width= 40)
-entry.focus_set()
-entry.pack()
+win = tk.Tk()
+win.eval('tk::PlaceWindow . center')
+win.title("Signing APP - BSK Project")
 
-#Create a Button to validate Entry Widget
-ttk.Button(win, text= "Generuj parę kluczy",width= 20, command=generateRSA).pack(pady=20)
+# Ustawienie wag dla kolumn i wierszy
+win.columnconfigure(0, weight=1)
+win.columnconfigure(1, weight=1)
+win.columnconfigure(2, weight=1)
+win.rowconfigure(0, weight=1)
+win.rowconfigure(1, weight=1)
+win.rowconfigure(2, weight=1)
 
+label1 = ttk.Label(win, text="Signing APP - BSK Project", font=("Arial", 14))
+label1.grid(row=0, column=1, pady=10)
+
+button1 = ttk.Button(win, text="Generuj parę kluczy", width=20, command=enterPIN)
+button1.grid(row=1, column=0, pady=20, padx=20)
+
+button2 = ttk.Button(win, text="Podpisz plik", width=20, command=generateRSA)
+button2.grid(row=1, column=2, pady=20, padx=20)
+
+label2 = ttk.Label(win, text="")
+label2.grid(row=2, column=1, pady=(0, 20))
+
+
+def on_resize(event):
+    # Tutaj możesz dodać kod reagujący na zmianę rozmiaru okna
+    pass
+
+win.bind("<Configure>", on_resize)
 win.mainloop()
+
+
+
+
