56640762fe212d8c24f4773394b83eca43b3a811
KaraitPL <karaitpl@gmail.com>
2024-04-18 11:06:07 +0200

    File and Key choose button

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os.path
 import tkinter as tk
 from tkinter import ttk
 from tkinter.ttk import *
@@ -27,10 +28,14 @@ def generateRSA():
     if len(entry.get()) == 4:
         generate(entry.get())
         pinWindow.destroy()
-        label2.config(text="Klucze wygenerowane poprawnie")
+        label2.config(text="Klucze wygenerowane poprawnie", foreground="green")
 
 
 def chooseFileAndKey():
+    global labelFile
+    global labelKey
+    global fileAndKeyWindow
+    Tk().withdraw()
     fileAndKeyWindow = Toplevel(win)
     win.eval(f'tk::PlaceWindow {str(fileAndKeyWindow)} center')
     fileAndKeyWindow.title("Choose File and Key")
@@ -41,23 +46,37 @@ def chooseFileAndKey():
     fileAndKeyWindow.rowconfigure(0, weight=1)
     fileAndKeyWindow.rowconfigure(1, weight=1)
     fileAndKeyWindow.rowconfigure(2, weight=1)
+
     Label(fileAndKeyWindow, text="Wybierz plik do podpisania oraz klucz").grid(row=0, column=1, pady=10)
     Button(fileAndKeyWindow, text="Wybierz plik", command=chooseFile).grid(row=1, column=0, padx=10, pady=(10, 0))
-    Button(fileAndKeyWindow, text="Wybierz klucz", command=chooseFile).grid(row=1, column=2, padx=10, pady=(10, 0))
-    labelFile = Label(fileAndKeyWindow, text="")
+    Button(fileAndKeyWindow, text="Wybierz klucz", command=chooseKeyFile).grid(row=1, column=2, padx=10, pady=(10, 0))
+    labelFile = Label(fileAndKeyWindow, text=fileString)
     labelFile.grid(row=2, column=0, padx=10, pady=(0, 10))
-    labelKey = Label(fileAndKeyWindow, text="")
-    labelKey.grid(row=2, column=0, padx=10, pady=(0, 10))
+    labelKey = Label(fileAndKeyWindow, text=keyString)
+    labelKey.grid(row=2, column=2, padx=10, pady=(0, 10))
 
 
 def chooseFile():
-    Tk().withdraw()
-    filename = askopenfilename()
-    print(filename)
+    global fileString
+    filePath = askopenfilename()
+    fileString = os.path.basename(filePath)
+    labelFile.config(text=fileString)
+    fileAndKeyWindow.focus_set()
 
 
+def chooseKeyFile():
+    global keyString
+    keyPath = askopenfilename()
+    keyString = os.path.basename(keyPath)
+    labelKey.config(text=keyString)
+    fileAndKeyWindow.focus_set()
+
+
+fileString = ""
+keyString = ""
 win = tk.Tk()
-win.eval('tk::PlaceWindow . center')
+#win.eval('tk::PlaceWindow . center')
+win.geometry("+720+400")
 win.title("Signing APP - BSK Project")
 
 # Ustawienie wag dla kolumn i wierszy
